50054c6111933cbd09c04c6f763c7f495c3ec556
Nathan Malone <nathanamalone@gmail.com>
2022-10-07 11:36:17 -0500

    end chap 5

--- request_handler.py
+++ request_handler.py
@@ -1,31 +1,83 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_metals, get_all_orders, get_all_sizes, get_all_styles
+from views import get_single_metal, get_single_order, get_single_size, get_single_style
+from views import create_order, delete_order
 
 class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        try:
+           id = int(path_params[2])
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+        return (resource, id)  # This is a tuple
 
     def do_GET(self):
         """Handles GET requests to the server """
         self._set_headers(200)
+        response = {}
 
-        if self.path == "/metals":
-            response = get_all_metals()
+        (resource, id) = self.parse_url(self.path)
 
+        if resource == "metals":
+            if id is not None:
+                response = get_single_metal(id)
+            else:
+                response = get_all_metals()
+        elif resource == "sizes":
+            if id is not None:
+                response = get_single_size(id)
+            else:
+                response = get_all_sizes()
+        elif resource == "styles":
+            if id is not None:
+                response = get_single_style(id)
+            else:
+                response = get_all_styles()
+        elif resource == "orders":
+            if id is not None:
+                response = get_single_order(id)
+            else:
+                response = get_all_orders()
+        
         else:
             response = []
 
         self.wfile.write(json.dumps(response).encode())
 
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server """
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = { "payload" : post_body }
-        self.wfile.write(json.dumps(response).encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_item = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "orders":
+            new_item = create_order(post_body)
+        
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_item).encode())
 
     def do_PUT(self):
         """Handles PUT requests to the server """
@@ -51,6 +103,21 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
+
+    def do_DELETE(self):
+    # Set a 204 response code
+        self._set_headers(204)
+
+    # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+    # Delete a single order from the list
+        if resource == "orders":
+            delete_order(id)
+
+    # Encode the new order and send in response
+        self.wfile.write("".encode())
+
 
 
 
